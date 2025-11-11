6e5aa97921d5df729e434cb529619c355a391b14
TachyonS256 <tachyon256@proton.me>
2025-11-11 13:43:25 -0700

    close to done, add tests next.

--- jane.py
+++ jane.py
@@ -1,36 +1,43 @@
 import hashlib
+import os
 
 def crack(hash_file,word_file):
     hashes = {}
-    output = set()
     with open(hash_file, 'r') as h:
         for line in h:
-            hashes.update({line.strip(),''})
+            hashes[line.strip()] = None
     with open(word_file, 'r') as w:
         for line in w:
             line = line.strip()
             hash_obj = hashlib.md5(line.encode())
             hash_hex = hash_obj.hexdigest()
             if hash_hex in hashes:
-                print(f'< + > -Cracked! {hash_hex} --> {line.strip}')
-                hashes.update({hash_hex,line.strip()})
-            else:
+                print(f'< + > -Cracked! {line.strip()} <-- {hash_hex}')
+                hashes.update({hash_hex:line.strip()})
+        for x in hashes:
+            if hashes[x] == None:
                 print(f'< x > -Failed! Could not find hash in words file. {hash_hex}')
-                hashes.update({hash_hex,'fail'})
-        print(output)
-        print(len(output))
-        return output
+                hashes.update({x:'Failed!'})
+        print(hashes)
+        print(len(hashes))
+        return hashes
 def main():
     print('--- Jane the Ripper ---')
     while True:
-        #hash_path = input('Path to hash file:')
-        #word_path = input('Path to word file:')
-
-        hash_path = 'hashes.txt'
-        word_path = 'wordlist.txt'
+        hash_path = input('Path to hash file:')
+        if not os.path.exists(hash_path):
+            print("Hash path invalid!")
+            main()
+        word_path = input('Path to word file:')
+        if not os.path.exists(word_path):
+            print("Word file path invalid!")
+            main()
+        #hash_path = 'hashes.txt'
+        #word_path = 'wordlist.txt'
 
         conf = input(f'-- You"ve entered \n Hash file: {hash_path} \n Word file: {word_path} \n Is this correct? (y/n) >')
         if conf == 'y' or conf == '':
             crack(hash_path, word_path)
             break
-main()
+if __name__ == "__main__":
+    main()
--- test_jane.py
+++ test_jane.py
@@ -1,4 +1,7 @@
 import jane
 
 def test_hash_file():
-    assert
+    assert jane.crack('hashes.txt','wordlist.txt') == {'a53f3929621dba1306f8a61588f52f55': 'edward', 'e99a18c428cb38d5f260853678922e03': 'abc123', '40be4e59b9a2a2b5dffb918c0e86b3d7': 'welcome', 'c33367701511b4f6020ec61ded352059': '654321', 'aa47f8215c6f30a0dcdb2a36a9f4168e': 'daniel', 'eb0a191797624dd3a48fa681d3061212': 'master', 'f4f068e71e0d87bf0ad51e6214ab84e9': 'angel', 'fe546279a62683de8ca334b673420696': 'samsung', 'bf779e0933a882808585d19455cd7937': 'charlie', '21b72c0b7adc5c7b4a50ffcb90d92dd6': 'matrix', 'ee7d692412c833694f6341aa54fb5ad9': 'Failed!', 'b9e851d3be78ab9ed830f47fb5dec294': 'Failed!', '9b66f29e6a3bf8f78003c3642305b7fa': 'Failed!', '7037f707e8b53a3d183c1b0b5031e7c0': 'Failed!', '74309fe83c9bc17a25ca74e69c3cf049': 'Failed!'}
+
+
+
